b676bf86816fba0f53cb4d1290517bec945abf1a
Connor-Gill <m.connor.gill@gmail.com>
2017-10-25 01:44:37 +0100

    second commit

---
+++ AttemptOne.py
@@ -0,0 +1,11 @@
+import urllib2, json
+#import Windows
+
+#print urllib2.urlopen("https://www.cat.com").read()
+
+jsonTest = urllib2.urlopen("https://graph.facebook.com/facebook/picture?redirect=false").read()
+
+Windows.security
+
+print json.loads(jsonTest).values()[0][u"url"]
+
